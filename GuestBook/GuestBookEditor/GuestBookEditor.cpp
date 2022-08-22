3a88e18c902d52e27ef507d79b1fb39ead0f146d
UnitySio <dnwls010728@gmail.com>
2022-08-22 15:40:30 +0900

    임시 업로드

--- GuestBook/GuestBookEditor/GuestBookEditor.cpp
+++ GuestBook/GuestBookEditor/GuestBookEditor.cpp
@@ -3,9 +3,12 @@
 
 #include "framework.h"
 #include "GuestBookEditor.h"
+#include "mmsystem.h"
 
 #include <string>
 #include <vector>
+
+#pragma comment(lib, "winmm.lib");
 
 using namespace std;
 
@@ -16,24 +19,28 @@ HINSTANCE hInst;                                // 현재 인스턴스입니다.
 WCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
 WCHAR szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름입니다.
 
+// 윈도우 크기
+const int windows_size_width = 1280;
+const int windows_size_height = 720;
+
+// 타임라인 진행률
+float progress = 0; // 현재 진행률
+float max_progress = 0; // 진행률 최대치 (추후 기록된 시간의 합으로 값을 적용하여 동작)
+
+UINT progress_timer;
+
+ULONGLONG create_time;
+ULONGLONG current_time;
+
+bool is_click = false;
+
 // 이 코드 모듈에 포함된 함수의 선언을 전달합니다:
 ATOM                MyRegisterClass(HINSTANCE hInstance);
 BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 
-// 윈도우 크기
-const int windows_size_width = 1280;
-const int windows_size_height = 720;
-
-// 프레임
-const int frame_rate = 60;
-
-// 타임라인 진행률
-float progress = 0; // 현재 진행률
-float max_progress = 13; // 진행률 최대치 (추후 기록된 시간의 합으로 값을 적용하여 동작)
-
-vector<ULONGLONG> times;
+void CALLBACK TimerProc(UINT uTimerID, UINT uMsg, DWORD_PTR dwUser, DWORD_PTR dw1, DWORD_PTR dw2);
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
@@ -145,19 +152,50 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     {
     case WM_CREATE:
         {
-            
+            create_time = (DWORD)GetTickCount64();
         }
         break;
     case WM_KEYDOWN:
         {
+            TIMECAPS timecaps;
+            timeGetDevCaps(&timecaps, sizeof(TIMECAPS));
+            
             switch (wParam)
             {
             case VK_LEFT:
-                SetTimer(hWnd, 1, 1000 / frame_rate, NULL);
+                if (progress_timer != 0)
+                {
+                    timeKillEvent(progress_timer);
+                }
+                
+                progress_timer = timeSetEvent(1, timecaps.wPeriodMax, TimerProc, (DWORD)hWnd, TIME_PERIODIC);
                 break;
             case VK_RIGHT:
-                KillTimer(hWnd, 1);
+                timeKillEvent(progress_timer);
+                progress_timer = 0;
                 break;
+            }
+        }
+        break;
+    case WM_LBUTTONUP:
+        {
+            is_click = false;
+        }
+        break;
+    case WM_LBUTTONDOWN:
+        {
+            is_click = true;
+        }
+        break;
+    case WM_MOUSEMOVE:
+        {
+            if (is_click)
+            {
+                current_time = (DWORD)GetTickCount64() - create_time;
+                max_progress = (float)current_time / 1000;
+
+                RECT r = {0, windows_size_height - 150, windows_size_width, windows_size_height - 140};
+                InvalidateRect(hWnd, &r, TRUE);
             }
         }
         break;
@@ -183,13 +221,19 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             PAINTSTRUCT ps;
             HDC hdc = BeginPaint(hWnd, &ps);
             // TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다...
-            WCHAR fps_word[1024];
             WCHAR progress_word[1024];
-            wsprintf(fps_word, L"FPS: %d", frame_rate);
-            TextOut(hdc, 0, 0, fps_word, lstrlen(fps_word));
-            Rectangle(hdc, 0, windows_size_height - 200, (progress / max_progress) * windows_size_width, windows_size_height - 190);
-            wsprintf(progress_word, L"Frame: %d / %d", (int)progress, (int)max_progress);
+            SetBkMode(hdc, TRANSPARENT);
+            _stprintf_s(progress_word, L"%.2fs / %.2fs", progress, max_progress);
             TextOut(hdc, 0, windows_size_height - 220, progress_word, lstrlen(progress_word));
+
+            HBRUSH n = (HBRUSH)CreateSolidBrush(RGB(255, 0, 0));
+            HBRUSH o = (HBRUSH)SelectObject(hdc, n);
+
+            Rectangle(hdc, 0, windows_size_height - 200, (progress / max_progress) * windows_size_width, windows_size_height - 190);
+
+            SelectObject(hdc, o);
+            DeleteObject(n);
+
             EndPaint(hWnd, &ps);
         }
         break;
@@ -197,15 +241,8 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         {
             switch (wParam)
             {
-            case 1:
-                progress++;
-                InvalidateRect(hWnd, NULL, TRUE);
-                
-                if (progress == max_progress)
-                {
-                    progress = 0;
-                }
-                break;
+                default:
+                    break;
             }
         }
         break;
@@ -236,4 +273,20 @@ INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
         break;
     }
     return (INT_PTR)FALSE;
+}
+
+void CALLBACK TimerProc(UINT uTimerID, UINT uMsg, DWORD_PTR dwUser, DWORD_PTR dw1, DWORD_PTR dw2)
+{
+    if (uTimerID == progress_timer)
+    {
+        RECT r = {0, windows_size_height - 220, windows_size_width, windows_size_height - 170};
+    
+        progress += 0.001;
+        InvalidateRect((HWND)dwUser, &r, TRUE);
+                
+        if (progress >= max_progress)
+        {
+            progress = 0;
+        }
+    }
 }
